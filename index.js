2e2da58a97db2d9b5173e86660544dd131cb57e1
Athul <athulcm771@gmail.com>
2024-06-19 16:54:11 +0530

    numemai

--- index.js
+++ index.js
@@ -339,12 +339,12 @@ async function sendWhatsAppMessage() {
 }
 
 // Schedule the task to run every day at 3:00 PM
-cron.schedule('50 16 * * *', () => {
-  console.log('Running cron job at 4:50 PM');
+cron.schedule('15 16 * * *', () => {
+  console.log('Running cron job at 4:15 PM');
   sendWhatsAppMessage();
 });
 
-console.log('Cron job scheduled to send WhatsApp message every day at 4:50 PM');
+console.log('Cron job scheduled to send WhatsApp message every day at 4:15 PM');
 
 
 
