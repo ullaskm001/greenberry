f4f81b849781744012080dbb60ac50d54f8d7d03
ULLAS K M <69137198+ullaskm001@users.noreply.github.com>
2024-08-08 20:18:24 +0530

    Update index.js new time

--- index.js
+++ index.js
@@ -371,7 +371,7 @@ function scheduleWhatsAppMessage(hours, minutes) {
 }
 
 // Schedule the message to be sent at a specific time
-scheduleWhatsAppMessage(20, 15); // Set the desired hours and minutes
+scheduleWhatsAppMessage(20, 40); // Set the desired hours and minutes
 
 // Start the server
 app.listen(port, () => {
